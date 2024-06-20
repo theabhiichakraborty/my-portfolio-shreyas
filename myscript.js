258b55d33bd45f3426cd486475cd56a769ab4617
CHAKRABORTY@ <157191264+theabhiichakraborty@users.noreply.github.com>
2024-06-20 17:52:58 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,39 @@
+const words = ["Tech Enthusiast", "Web Developer", "Programmer"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('word').innerHTML += word.shift();
+		} else {
+			setTimeout(deletingEffect, 1700);
+			return false;
+		};
+		timer = setTimeout(loopTyping, 100);
+	};
+	loopTyping();
+};
+
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('word').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect() ;
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 100);
+	};
+	loopDeleting();
+};
+
+typingEffect();
